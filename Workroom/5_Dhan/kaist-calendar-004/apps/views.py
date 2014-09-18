265a7b90d6e757cad4b642c484ac746c46697d95
Han Dongsik <han.dongsik@gmail.com>
2014-09-18 10:17:17 +0900

    Database and CRUD

--- Workroom/5_Dhan/kaist-calendar-004/apps/views.py
+++ Workroom/5_Dhan/kaist-calendar-004/apps/views.py
@@ -32,7 +32,6 @@ def event_create():
                 poster = form.poster.data,
                 contact = form.contact.data,
                 contact_open = form.contact_open.data,
-                password = form.password.data
             )
 
             db.session.add(event)
@@ -63,41 +62,52 @@ def event_update(id):
 
     return render_template('event/update.html', form=form)
 
+@app.route('/event/delete/<int:id>', methods=['GET', 'POST'])
+def event_delete(id):
+    if request.method == 'GET':
+        return render_template('event/delete.html', event_id=id)
+    elif request.method == 'POST':
+        event_id = request.form['event_id']
+        event = Event.query.get(event_id)
+        db.session.delete(event)
+        db.session.commit()
 
+        flash(u'게시글을 삭제하였습니다.', 'success')
+        return redirect(url_for('event_list'))
 
-@app.route('/get_inform')
-def get_inform():
+# @app.route('/get_inform')
+# def get_inform():
 
-	id = request.args.get('id', 0, type=int)
+# 	id = request.args.get('id', 0, type=int)
 
-	inform = {}
-	inform['id'] = 1
-	inform['title'] = '동틀무렵 7080 나이트'
-	inform['content'] = '안녕하세요, KAIST 동틀무렵입니다. 잘 부탁드립니다'
-	inform['host'] = '동틀무렵'
-	inform['category_char'] = '공연'
-	inform['category_host'] = '동아리'
-	inform['date_start'] = '140903 19:00'
-	inform['date_end'] = '140903 22:00'
-	inform['location'] = '미래홀'
+# 	inform = {}
+# 	inform['id'] = 1
+# 	inform['title'] = '동틀무렵 7080 나이트'
+# 	inform['content'] = '안녕하세요, KAIST 동틀무렵입니다. 잘 부탁드립니다'
+# 	inform['host'] = '동틀무렵'
+# 	inform['category_char'] = '공연'
+# 	inform['category_host'] = '동아리'
+# 	inform['date_start'] = '140903 19:00'
+# 	inform['date_end'] = '140903 22:00'
+# 	inform['location'] = '미래홀'
 
-	return jsonify(inform = inform)
+# 	return jsonify(inform = inform)
 
-@app.route('/get_month')
-def get_month():
-	month = request.args.get('month', 0, type=int)
-	return jsonify(month = month)
+# @app.route('/get_month')
+# def get_month():
+# 	month = request.args.get('month', 0, type=int)
+# 	return jsonify(month = month)
 
-@app.route('/get_week')
-def get_week():
-	week = request.args.get('week', 0, type=int)
-	return jsonify(week = week)
+# @app.route('/get_week')
+# def get_week():
+# 	week = request.args.get('week', 0, type=int)
+# 	return jsonify(week = week)
 
-@app.route('/categorize')
-def categorize():
-	category = request.args.get('category','None',type=str)
-	# selected_events = Event.query.filter(Event.category_host == category)
-	return jsonify(category = category)
+# @app.route('/categorize')
+# def categorize():
+# 	category = request.args.get('category','None',type=str)
+# 	# selected_events = Event.query.filter(Event.category_host == category)
+# 	return jsonify(category = category)
 
 
 #
